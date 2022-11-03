123f2a56ade3d2771d18a763db24b537bc793df8
Seyma Helin Kaya <98790266+shelinkaya@users.noreply.github.com>
2022-11-03 13:57:33 +0100

    Update random-fullfilled-circles_with_random_colors.py

--- random-fullfilled-circles_with_random_colors.py
+++ random-fullfilled-circles_with_random_colors.py
@@ -1,33 +1,75 @@
-from turtle import Screen, Turtle
+#below code draw 100 randomized fullfilled circles with random colors and save as .svg and .json
+
 from random import randint
+from svg_turtle import SvgTurtle
+import turtle
+from turtle import Turtle
+import turtle 
+import xml.etree.ElementTree as ET
+import json
+from turtle import Screen
 
-FIBER_RADIUS = 25
-FIBER_NUMBER = 100
-CURSOR_SIZE = 20
+screen= Screen()
 
-def fiber_circle(fiber):
-    r = randint(0, 255)
-    g = randint(0, 255)
-    b = randint(0, 255)
 
-    x = randint(FIBER_RADIUS - width//2, width//2 - FIBER_RADIUS)
-    y = randint(FIBER_RADIUS - height//2, height//2 - FIBER_RADIUS)
+def fiber_circle(fiber, width, height):
+    fiber_r=25
+    fiber_num = 100
+    cursor_size = 20
+    fiber.hideturtle()
+    fiber.speed("fastest")
+    fiber.screen.colormode(255)
+    
+    fiber.screen.bgcolor("white")
+    fiber.shape("circle")
+    fiber.shapesize(fiber_r / cursor_size)
+    fiber.penup()
+    fibers = [] 
+    
+    for _ in range(fiber_num):
+        fiber.color(randint(0, 255),randint(0, 255),randint(0, 255))
+        fiberr = fiber.clone()
+        fiberr.setposition(
+            randint(-width / 2, width / 2),
+            randint(-height / 2, height / 2),
+        )
 
-    fiber.color(r, g, b)
-    fiber.goto(x, y)
-    fiber.dot(FIBER_RADIUS * 2)  # dot() takes a diameter
+        while any((a.distance(fiberr) < fiber_r for a in fibers)):
+            fiberr.setposition(
+                randint(- width / 2, width / 2 ),
+                randint(- height / 2, height / 2),
+            )
+        
+        fiberr.stamp()
+        
+        fibers.append(fiberr) 
+def write_file(fiber_circle, filename, width, height):
+    fiber = SvgTurtle(width, height)
+    
+    fiber_circle(fiber, width, height)
+    # Convert to svg string
+    data = fiber.to_svg()
+    
+    # get the String data
+    root = ET.fromstring(data)
+     # loop through all entries
+    outfile = {}
+    outfile['polygons'] = []
+    for child in root:
+        # find tag that consits of "polygon"
+        if 'polygon' in child.tag:
+            outfile['polygons'].append(child.attrib.get('points'))
+    with open((filename+".json"),'w') as outjson:
+        json.dump(outfile,outjson)
 
-screen = Screen()
-screen.colormode(255)
 
-width, height = screen.window_width(), screen.window_height()
+    
 
-fiber = Turtle()
-fiber.hideturtle()
-fiber.speed("fastest")
-fiber.penup()
+    fiber.save_as((filename+".svg"))
 
-for _ in range(FIBER_NUMBER):
-    fiber_circle(fiber)
-
-screen.exitonclick()
+def main():
+    write_file(fiber_circle, "image15", 500, 500)
+    print("Done.")
+    
+if __name__ == "__main__":
+    main()
